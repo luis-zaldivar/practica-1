96fe025c7dbc1f58d90222a352d972e46a935f53
luis-zaldivar <lalozaldivarbarza@gmail.com>
2019-09-11 20:45:34 -0500

    utilizacion de punteros con arrelgos

--- main.cpp
+++ main.cpp
@@ -1,19 +1,39 @@
-#include <iostream>
-#include <stdlib.h>
+#include <bits/stdc++.h>
 using namespace std;
-struct Person{
-    char nombre[20];
+struct equipo{
+    char region[22];
+    char nombre[22];
+};
+struct jugador{
+    char nombre[22];
     int edad;
-    }Person1,Person2;
+    equipo equipo;
+};
 
 int main()
 {
-    cout <<"nombre: ";
-    cin.getline(Person1.nombre,20,'\n');
-    cout<<"edad: ";
-    cin>>Person1.edad;
-    cout<<Person1.nombre<<endl;
-    cout<<Person1.edad<<endl;
-    system("pause");
+    jugador personas[11];
+    jugador *apuntador=personas;
+    for(int i=0;i<=1;i++){
+        cout<<"Nombre: ";
+        cin.getline(personas[i].nombre,20);
+        cout<<"Edad: ";
+        cin>>personas[i].edad;
+        cout<<"region: ";
+        fflush(stdin);
+        cin.getline(personas[i].equipo.region,20);
+        cout<<"nombre del equipo: ";
+        cin.getline(personas[i].equipo.nombre,20);
+    }
+    cout<<endl;
+    for(;apuntador<=&personas[1];apuntador++){
+        cout<<"Nombre: "<<apuntador->nombre<<endl;
+        cout<<"Edad: "<<apuntador->edad<<endl;
+        cout<<"equipo:  "<<apuntador->equipo.region<<endl;
+        cout<<"nombre del equipo: "<<apuntador->equipo.nombre<<endl;
+        cout<<endl;
+    }
+
     return 0;
 }
+
